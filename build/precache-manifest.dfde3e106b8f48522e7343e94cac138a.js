65c0522e1cb3eed04bc75e7ecd33febfa08fcbd2
DamjanKarlovski <damjan_karlovski@hotmail.com>
2020-09-05 14:31:09 +0200

    RoboFriends

---
+++ build/precache-manifest.dfde3e106b8f48522e7343e94cac138a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b295657b71f8777c07e84af655cfa9a",
+    "url": "/DamjanKarlovski.github.io/robofriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/DamjanKarlovski.github.io/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "92b30a55be8cc6d17a24",
+    "url": "/DamjanKarlovski.github.io/robofriends/static/css/main.6ad65f18.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/DamjanKarlovski.github.io/robofriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DamjanKarlovski.github.io/robofriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92b30a55be8cc6d17a24",
+    "url": "/DamjanKarlovski.github.io/robofriends/static/js/main.8e8f5891.chunk.js"
+  },
+  {
+    "revision": "f194836cc7a848fd2fbe",
+    "url": "/DamjanKarlovski.github.io/robofriends/static/js/runtime-main.4ab267ae.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/DamjanKarlovski.github.io/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
